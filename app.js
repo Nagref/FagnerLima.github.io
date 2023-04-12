0f30aaf07c8d3d1e26eed1271942307c6f31ee62
Nagref <fagnerrlima@outlook.com>
2023-04-12 19:33:42 -0300

    update firebase

--- app.js
+++ app.js
@@ -1,42 +1,25 @@
-// Imports
-const express = require('express')
-const app = express()
-const port = 5000
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCOtEMDKPCCaRP29Tu_Km5kRnNgtY7Dmnw",
+  authDomain: "hi-fagner-lima.firebaseapp.com",
+  databaseURL: "https://hi-fagner-lima-default-rtdb.firebaseio.com",
+  projectId: "hi-fagner-lima",
+  storageBucket: "hi-fagner-lima.appspot.com",
+  messagingSenderId: "95900420741",
+  appId: "1:95900420741:web:82a282e2ea1ed7d307f7ec",
+  measurementId: "G-4VJE8T1BDS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 // Static Files
 app.use(express.static('public'));
 // Specific folder example
-// app.use('/css', express.static(__dirname + 'public/css'))
-// app.use('/js', express.static(__dirname + 'public/js'))
-// app.use('/img', express.static(__dirname + 'public/images'))
-
-// Set View's
-app.set('views', './views');
-app.set('view engine', 'ejs');
-
-// Navigation
-app.get('', (req, res) => {
-    res.render('pages/index', { text: 'Hey' })
-})
-
-app.get('/about', (req, res) => {
-   res.sendFile(__dirname + '/views/about.html'),
-   res.render('pages/about')
-})
-
-app.get('/uses', (req, res) => {
-   res.sendFile(__dirname + '/views/uses.html'),
-   res.render('pages/uses')
-})
-
-app.get('/game', (req, res) => {
-   res.sendFile(__dirname + '/views/game.html'),
-   res.render('pages/game')
-})
-
-app.get('/contact', (req, res) => {
-   res.sendFile(__dirname + '/views/contact.html'),
-   res.render('pages/contact')
-})
-
-app.listen(port, () => console.info(`App listening on port ${port}`))
