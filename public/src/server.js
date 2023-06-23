7e122fa4406c0cd18e6c8c0dbbce490d626eb832
Nagref <fagnerrlima@outlook.com>
2023-06-23 16:21:44 -0300

    create test files

--- public/src/server.js
+++ public/src/server.js
@@ -1,7 +1,7 @@
 import http from 'node:http'
 
 const server = http.createServer(async (req, res) => {
-  return res.end('Hello, world')
+  return res.end('Hello, world!')
 })
 
 server.listen(3333)
