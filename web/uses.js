59c84a201a23fffd9d7a0b4f9a2df2dec249036f
Nagref <fagnerrlima@outlook.com>
2023-07-28 11:14:33 -0300

    update uses add js

--- web/uses.js
+++ web/uses.js
@@ -1,9 +1,29 @@
 var img = document.getElementById('setupimg');
-img.addEventListener("mouseenter", entrar)
-img.addEventListener("mouseout", sair)
-function entrar(){
-  img.style = "max-width: 100%"
+img.addEventListener("click", focar)
+img.addEventListener("dblclick", desfocar)
+function focar(){
+  // img.style = "max-width: 100%"
+  if (img.requestFullScreen) {
+  img.requestFullScreen()
+} else if (img.webkitRequestFullScreen) {
+  img.webkitRequestFullScreen()
+} else if (img.mozRequestFullScreen) {
+  img.mozRequestFullScreen()
+} else {
+  console.log("O navegador não suporta a tela cheia")
 }
-function sair(){
-  img.style = "max-width: 30%"
+}
+function desfocar(){
+  // img.style = "max-width: 30%"
+  if (img.exitFullscreen) {
+    img.exitFullscreen()
+  } else if (img.webkitExitFullscreen) {
+    img.webkitExitFullscreen()
+  } else if (img.mozCancelFullScreen) {
+    img.mozCancelFullScreen()
+  } else if (img.msExitFullscreen) {
+    img.msExitFullscreen()
+  } else {
+    console.log("Não foi possível sair da tela cheia")
+  }
 }
