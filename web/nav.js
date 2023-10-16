b6ff05dd4fc33c7875b61e7b9ae0efd1ae31d405
Nagref <fagnerrlima@outlook.com>
2023-10-16 14:33:20 -0300

    update ajax 3.7.1

--- web/nav.js
+++
@@ -1,9 +0,0 @@
-const currentPageUrl = window.location.href;
-const menuItems = document.querySelectorAll("nav a")
-menuItems.forEach(item => {
-  const itemUrl = item.href ;
-
-  if(currentPageUrl === itemUrl) {
-    item.classList.add("active");
-  }
-})
